a117b08a62688d22c61e977d5d689e3e8f9d7e46
Nandhamaddy007 <80019057+Nandhamaddy007@users.noreply.github.com>
2021-05-09 22:08:05 +0530

    added post for createshop and update shop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,82 @@
 //var http = require("http");
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+var CryptoJS = require("crypto-js");
 let app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(function (req, res, next) {
+  //res.header("Access-Control-Allow-Origin",heroku);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Experss reply");
 });
-app.get("/hello", (req, res) => {
-  res.send("Hello reply");
+app.get("/GetShop", (req, res) => {
+  console.log("data got");
+  res.send({
+    shopName: "Ak stores",
+    shopOwner: "Arun kumar from backend",
+    shopOwnerMobile: "9876543123",
+    shopOwnerEmail: "aK@GMAIL.Com",
+    shopOwnerAddress: "Chennai",
+    shopOwnerInstaId: "Akstores@insta",
+    shopOwnerGpay: "123456",
+    shopOwnerPaytm: "9877654",
+    shopLogo: "",
+    ProductDetails: [
+      {
+        productName: "shirt",
+        productDescription: "sdsd",
+        productColor: "sdsdsd",
+        ProductVariance: [
+          {
+            productPrice: "499",
+            productAvailability: true,
+            productSize: "32"
+          },
+          {
+            productPrice: "599",
+            productAvailability: true,
+            productSize: "38"
+          }
+        ]
+      },
+      {
+        productName: "pant",
+        productDescription: "dfkkjf",
+        productColor: "blue",
+        ProductVariance: [
+          {
+            productPrice: "799",
+            productAvailability: true,
+            productSize: "32"
+          },
+          {
+            productPrice: "899",
+            productAvailability: true,
+            productSize: "36"
+          }
+        ]
+      }
+    ]
+  });
+});
+app.post("/CreateShop", (req, res) => {
+  //console.log(req.body);
+  let bytes = CryptoJS.AES.decrypt(req.body.body, "!@#$%^&*()");
+  let key = bytes.toString(CryptoJS.enc.Utf8);
+  let shopData = JSON.parse(key);
+  console.log(shopData);
+  res.send(shopData);
 });
 //create a server object:
 app.listen(8080, () => console.log("Server started"));
